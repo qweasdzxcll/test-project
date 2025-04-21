7eddd66e7ab5080f714946e798ff167919f83420
qweasdzxcll <denispodmogilnyj@gmail.com>
2025-04-21 14:02:41 +0300

    fix

--- frontend/src/widgets/ListTasks/ui/ListTasks.tsx
+++ frontend/src/widgets/ListTasks/ui/ListTasks.tsx
@@ -2,14 +2,13 @@ import { Task } from '../../../entities/'
 import { ITasks } from '../../../entities/Task'
 import { useContext } from 'react'
 import { TasksContext } from '../../../app/context/TasksContext'
-import styles from './ListTasks.module.scss'
 
 export const ListTasks = ({ marks }: ITasks) => {
 
   const { tasks } = useContext(TasksContext)
 
   return (
-    <div className={styles.list_tasks}>
+    <div>
       {tasks && marks &&
         tasks.map(task => <Task marks={marks} task={task} key={task.title} />)
       }
